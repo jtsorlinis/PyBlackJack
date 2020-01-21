3c8ab3103f653f878a3e4b639ab95ea6cabb88af
jtsorlinis <jtsorlinis@gmail.com>
2020-01-22 02:34:47 +1100

    finished the fix

--- card.py
+++ card.py
@@ -21,16 +21,13 @@ class Card:
             return self.rank
     
     def count(self):
-        if(self.faceDown):
+        if self.rank in [10, "J", "Q", "K", "A"]:
+            return -1
+        elif self.rank in [7,8,9]:
             return 0
+        elif self.rank in [2,3,4,5,6]:
+            return 1
         else:
-            if self.rank in [10, "J", "Q", "K", "A"]:
-                return -1
-            elif self.rank in [7,8,9]:
-                return 0
-            elif self.rank in [2,3,4,5,6]:
-                return 1
-            else:
-                print("erored with " + str(self.rank))
-                exit()
+            print("erored with " + str(self.rank))
+            exit()
         
