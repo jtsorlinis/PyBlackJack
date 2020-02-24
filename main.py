9512a807b93fc3bd9c85fc3694a8f71d870a13ff
jtsorlinis <jtsorlinis@gmail.com>
2020-02-24 22:01:59 +1100

    renamed functions in main

--- main.py
+++ main.py
@@ -4,33 +4,33 @@ from table import Table
 
 def main():
 
-    PLAYERS = 5
-    DECKS = 8
-    BET_SIZE = 10
-    MIN_CARDS = 40
+    players = 5
+    decks = 8
+    bet_size = 10
+    min_cards = 40
 
-    ROUNDS = 1000000
-    VERBOSE = 0
+    rounds = 1000000
+    verbose = 0
 
     if len(sys.argv) == 2:
-        ROUNDS = int(sys.argv[1])
+        rounds = int(sys.argv[1])
 
-    if VERBOSE and ROUNDS > 100:
+    if verbose and rounds > 100:
         sys.stdout = open("output.txt", "w")
 
-    T = Table(PLAYERS, DECKS, BET_SIZE, MIN_CARDS, VERBOSE)
-    T.cardpile.shuffle()
+    table1 = Table(players, decks, bet_size, min_cards, verbose)
+    table1.cardpile.shuffle()
 
-    START_TIME = time.perf_counter()
-    for x in range(0, ROUNDS):
-        if VERBOSE:
-            print("Round " + str(x + 1))
-        if not VERBOSE and ROUNDS > 1000 and x % (ROUNDS / 100) == 0:
-            print("\tProgress: " + str(int(x * 100 / ROUNDS)), end="%\r")
-        T.start_round()
-        T.check_earnings()
+    start_time = time.perf_counter()
+    for _x in range(0, rounds):
+        if verbose:
+            print("Round " + str(_x + 1))
+        if not verbose and rounds > 1000 and _x % (rounds / 100) == 0:
+            print("\tProgress: " + str(int(_x * 100 / rounds)), end="%\r")
+        table1.start_round()
+        table1.check_earnings()
 
-    for player in T.players:
+    for player in table1.players:
         if not player.split_from:
             print(
                 "Player "
@@ -38,18 +38,17 @@ def main():
                 + " earnings: "
                 + str(player.earnings)
                 + "\t\tWin percentage: "
-                + str(50 + (player.earnings / (ROUNDS * BET_SIZE) * 50))
+                + str(50 + (player.earnings / (rounds * bet_size) * 50))
             )
-    print("Casino earnings: " + str(T.casino_earnings))
+    print("Casino earnings: " + str(table1.casino_earnings))
 
     print(
         "Played "
-        + str(x + 1)
+        + str(_x + 1)
         + " rounds in "
-        + format(time.perf_counter() - START_TIME, ".2f")
+        + format(time.perf_counter() - start_time, ".2f")
         + " seconds"
     )
 
 if __name__ == "__main__":
     main()
-    
